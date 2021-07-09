f25fdc1a0c30306ec528be1a4b148ae438f7492f
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-10 01:40:57 +0600

    Create PythonProgram15.py

---
+++ PythonProgram15.py
@@ -0,0 +1,17 @@
+'''
+Relational Operators & Boolean Data Types
+>
+>=
+<
+<=
+==
+!=
+a = b [ store ]
+a == b [ equality check ]
+'''
+print(30>20)
+print(30<20)
+print(30>=20)
+print(30<=20)
+print(30!=6)
+print(30==20)
